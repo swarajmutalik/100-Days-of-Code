3e850cf842eb059e310ccc3bbfcd5cace4c75b46
SWARAJ RAJENDRA MUTALIK <swarajmutalik@gmail.com>
2024-01-18 19:00:53 +0530

    Number is even or odd

---
+++ Even_or_Odd/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int num = 10;
+        if(num % 2 == 0){
+            System.out.println("The number is even");
+        }
+        else {
+            System.out.println("The number is odd");
+        }
+    }
+}
