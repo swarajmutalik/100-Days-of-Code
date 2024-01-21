770a541c3c7fda67935e628cf80ff0099726ecc2
SWARAJ RAJENDRA MUTALIK <swarajmutalik@gmail.com>
2024-01-21 11:46:16 +0530

    Reversing a Number

---
+++ Reverse_a_Number/src/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number:");
+        int num = sc.nextInt();
+        int rev = 0, rem;
+        while (num != 0) {
+            rem = num % 10;
+            rev = rev * 10 + rem;
+            num = num / 10;
+        }
+        System.out.println("The number after reversing is " + rev);
+    }
+}
