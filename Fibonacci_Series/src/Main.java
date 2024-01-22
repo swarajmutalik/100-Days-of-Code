dacb9eba897a7b4fa69b2ecb5144f6c1e719c6a7
SWARAJ RAJENDRA MUTALIK <swarajmutalik@gmail.com>
2024-01-22 19:42:31 +0530

    Fibonacci Series

---
+++ Fibonacci_Series/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 0; int b = 1;
+        int c;
+        for(int i = 1; i<=10; i++){
+            c = a + b;
+            System.out.print(c + " ");
+            a = b;
+            b = c;
+        }
+    }
+}
