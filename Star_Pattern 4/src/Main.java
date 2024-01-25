31e41a6334824ed35b13f26d2be71ed28621ff37
SWARAJ RAJENDRA MUTALIK <swarajmutalik@gmail.com>
2024-01-25 21:27:20 +0530

    Star Pattern 4

---
+++ Star_Pattern 4/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 4; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 4; k >= i; k--) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
