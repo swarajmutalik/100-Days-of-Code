021110111ff5aba52352bce244f36151ac55b848
SWARAJ RAJENDRA MUTALIK <swarajmutalik@gmail.com>
2024-01-28 20:09:40 +0530

    Arrays

---
+++ Arrays/src/Main.java
@@ -0,0 +1,30 @@
+public class Main {
+    public static void main(String[] args) {
+//        Printing the elements of a 1-D Array
+//        int a[] = {10,20,30};
+//        for(int i = 0; i<a.length; i++){
+//            System.out.println(a[i]);
+//        }
+
+//        Printing the elements of a 2-D Array
+//        int a [][] = {{10,20},{30,40,50}};
+//        for(int i = 0; i < a.length; i++){
+//            for(int j = 0; j < a[i].length; j++){
+//                System.out.print(a[i][j] + " ");
+//            }
+//            System.out.println();
+//        }
+
+//        Printing the elements of a 3-D Array
+//        int a [][][] = {{{10,20},{30,40,50}},{{60,70,80},{90,100}}};
+//        for(int i = 0; i<a.length; i++){
+//            for(int j = 0; j<a[i].length; j++){
+//                for(int k = 0; k<a[i][j].length; k++){
+//                    System.out.print(a[i][j][k] + " ");
+//                }
+//                System.out.println();
+//            }
+//            System.out.println();
+//        }
+    }
+}
