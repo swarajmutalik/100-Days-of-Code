e82bdcdb1f1f21a8d9e6f09ac40172e47d2df481
SWARAJ RAJENDRA MUTALIK <swarajmutalik@gmail.com>
2024-02-01 19:53:46 +0530

    Binary Search

--- Binary Search/src/Main.java
+++ Binary Search/src/Main.java
@@ -1,5 +1,24 @@
 public class Main {
     public static void main(String[] args) {
+        int a[] = {1, 2, 3, 4, 5, 7, 8, 9};
+        int search = 5;
+        int li = 0;
+        int hi = a.length - 1;
+        int mi = (li + hi) / 2;
 
+        while (li <= hi) {
+            if (a[mi] == search) {
+                System.out.println("Element is at " + mi + " index position");
+                break;
+            } else if (a[mi] < search) {
+                li = mi + 1;
+            } else {
+                hi = mi - 1;
+            }
+            mi = (li + hi) / 2;
+        }
+        if (li > hi) {
+            System.out.println("Element not found in the list");
+        }
     }
 }
