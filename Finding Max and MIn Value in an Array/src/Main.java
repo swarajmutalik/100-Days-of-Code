48676b0b926355298b1f7b901cb6ed9747dbbf16
SWARAJ RAJENDRA MUTALIK <swarajmutalik@gmail.com>
2024-02-02 10:39:49 +0530

    Max and Min Value in an Array

---
+++ Finding Max and MIn Value in an Array/src/Main.java
@@ -0,0 +1,25 @@
+public class Main {
+    public static void main(String[] args) {
+//        int a [] = {4,3,1,2,5,6};
+
+//  Finding the Maximum  Value in an Array
+
+//        int max = a[0];
+//        for(int i = 1; i < a.length; i++){
+//            if(a[i] > max){
+//                max = a[i];
+//            }
+//        }
+//        System.out.println("Maximum value is " + max);
+
+//        Finding the Minimum Value in an Array
+
+//        int min = a[0];
+//        for(int i = 0; i < a.length; i++){
+//            if(min > a[i]){
+//                min = a[i];
+//            }
+//        }
+//        System.out.println("Minimum Value is " + min);
+    }
+}
