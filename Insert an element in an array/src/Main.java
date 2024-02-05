2563d2915f1adeab7c8f576c74199fa1a10c5921
SWARAJ RAJENDRA MUTALIK <swarajmutalik@gmail.com>
2024-02-05 19:34:40 +0530

    Insertion of an element in an array

---
+++ Insert an element in an array/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        int a [] = {10,20,30,40,50};
+        int pos = 3;
+        int element = 100;
+        for(int i = a.length - 1; i> pos - 1; i--){
+            a[i] = a[i - 1];
+        }
+        a[pos - 1] = element;
+        for(int i = 0; i < a.length; i++){
+            System.out.println(a[i]);
+        }
+    }
+}
