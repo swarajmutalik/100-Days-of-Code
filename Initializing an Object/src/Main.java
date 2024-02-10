1aca88787805b09a1063874220107fdcf5548be2
SWARAJ RAJENDRA MUTALIK <swarajmutalik@gmail.com>
2024-02-10 12:52:58 +0530

    Ways to Initialize an Object

---
+++ Initializing an Object/src/Main.java
@@ -0,0 +1,35 @@
+// Initializing an object by using a reference variable
+
+//public class Main {
+//    String color;
+//    int age;
+//    public static void main(String[] args) {
+//        Main m = new Main();
+//        m.color = "black";
+//        m.age = 10;
+//
+//        System.out.print(m.color + " " + m.age);
+//    }
+//}
+
+// Initializing an object using a method
+
+//public class Main {
+//    String color;
+//    int age;
+//
+//    void initObj(String c, int a) {
+//        color = c;
+//        age = a;
+//    }
+//
+//    void display() {
+//        System.out.print(color + " " + age);
+//    }
+//
+//    public static void main(String[] args) {
+//        Main m = new Main();
+//        m.initObj("black", 10);
+//        m.display();
+//    }
+//}
