8f790147e0817a360ea222377dd39a947a376494
SWARAJ RAJENDRA MUTALIK <swarajmutalik@gmail.com>
2024-02-11 14:06:34 +0530

    Introduction to Constructors

---
+++ Constructors/src/Main.java
@@ -0,0 +1,36 @@
+// default constructor
+
+//public class Main {
+//    int i;
+//    public static void main(String[] args) {
+//        Main m = new Main();
+//        System.out.println(m.i);
+//        }
+//    }
+
+//No argument constructor
+
+//public class Main{
+//    int i;
+//    String s;
+//
+//    Main (){
+//        System.out.println("no arg constructor");
+//    }
+//    public static void main(String[] args) {
+//        Main m = new Main();
+//        System.out.println(m.i + " " + m.s);
+//    }
+//}
+
+// parameterized constructor
+
+//public class Main{
+//    Main(int a){
+//        System.out.println("parameterized constructor");
+//    }
+//    public static void main(String[] args) {
+//        Main m = new Main(10);
+//
+//    }
+//}
