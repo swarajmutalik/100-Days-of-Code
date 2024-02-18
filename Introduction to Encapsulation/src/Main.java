532e90ac4d2f27a9a8538f093e7576d6b9ae19bd
SWARAJ RAJENDRA MUTALIK <swarajmutalik@gmail.com>
2024-02-18 18:02:42 +0530

    Introduction to encapsulation

--- Introduction to Encapsulation/src/Main.java
+++ Introduction to Encapsulation/src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
-    public static void main(String[] args) {
+   private int emp_id;
 
+   public void setEmp_id(int eid){
+       emp_id = eid;
+   }
+
+   public int getEmp_id(){
+       return emp_id;
+   }
+}
+
+class Company {
+    public static void main(String[] args) {
+        Main m = new Main();
+        m.setEmp_id(101);
+        System.out.println(m.getEmp_id());
     }
 }
+
