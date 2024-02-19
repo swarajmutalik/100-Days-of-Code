9530122d5a6dad1c95067a7ee12a03be4f868aa6
SWARAJ RAJENDRA MUTALIK <swarajmutalik@gmail.com>
2024-02-19 19:34:33 +0530

    this keyword

--- Introduction to this keyword/src/Main.java
+++ Introduction to this keyword/src/Main.java
@@ -1,5 +1,18 @@
+class Test {
+    int i;
+    void setValue(int i){
+        this.i = i;
+    }
+
+    void show(){
+        System.out.println(i);
+    }
+}
+
 public class Main {
     public static void main(String[] args) {
-
+        Test t = new Test();
+        t.setValue(10);
+        t.show();
     }
 }
