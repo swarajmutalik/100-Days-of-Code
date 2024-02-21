c49c8f149258399c172ef8f05b5a57ef5bc9efb2
SWARAJ RAJENDRA MUTALIK <swarajmutalik@gmail.com>
2024-02-21 11:00:56 +0530

    super keyword

--- Introduction to super keyword/src/Main.java
+++ Introduction to super keyword/src/Main.java
@@ -1,5 +1,16 @@
-public class Main {
+class A {
+    int a = 10;
+}
+
+class Main extends A {
+
+    int a = 20;
+    void show(int i){
+//        System.out.println(this.a);
+        System.out.println(super.a);
+    }
     public static void main(String[] args) {
-      
+      Main m = new Main();
+      m.show(30);
     }
 }
