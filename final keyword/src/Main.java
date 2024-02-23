7e8557fc9472b525a83f89bcde25df01bdce140a
SWARAJ RAJENDRA MUTALIK <swarajmutalik@gmail.com>
2024-02-23 11:08:11 +0530

    final keyword

---
+++ final keyword/src/Main.java
@@ -0,0 +1,37 @@
+//public class Main {
+//    public static void main(String[] args) {
+//        final int i = 10;
+//        i = i + 20;
+//        System.out.println(i);
+//    }
+//}
+
+
+
+//class Demo {
+//    final void m1(){
+//        System.out.println("I am in class Demo");
+//    }
+//}
+//
+//class Main extends Demo {
+////    void m1(){
+////        System.out.println("I am in class Main");
+////    }
+//
+//    public static void main(String[] args) {
+//
+//    }
+//}
+
+
+
+final class Demo {
+
+}
+
+class Main extends Demo {
+    public static void main(String[] args) {
+
+    }
+}
