033d926140e9b9813e5e814450f1b287435f3d4a
SWARAJ RAJENDRA MUTALIK <swarajmutalik@gmail.com>
2024-02-26 21:09:30 +0530

    Exception Handling

--- Exception Handling/src/Main.java
+++ Exception Handling/src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        
+        System.out.println("1");
+
+//        The below line gives an exception and stops the flow of code.
+        System.out.println(100 / 0);
+        System.out.println("2");
     }
 }
