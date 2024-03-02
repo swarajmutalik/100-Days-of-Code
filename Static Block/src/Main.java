d991a2a396ddb6e08ddf5320b005dc5dee03ff55
SWARAJ RAJENDRA MUTALIK <swarajmutalik@gmail.com>
2024-03-02 19:26:42 +0530

    Static Block

---
+++ Static Block/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    static {
+        System.out.println("Hello");
+    }
+    public static void main(String[] args) {
+        System.out.println("In the main method");
+    }
+}
