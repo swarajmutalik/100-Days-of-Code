01fe0f517cdf0e49beaf4338ca9400c2f4702fe7
SWARAJ RAJENDRA MUTALIK <swarajmutalik@gmail.com>
2024-03-03 17:13:20 +0530

    try catch

--- try catch/src/Main.java
+++ try catch/src/Main.java
@@ -1,3 +1,4 @@
+// Different methods of printing exception information
 public class Main {
     public static void main(String[] args) {
         try {
@@ -6,7 +7,14 @@ public class Main {
             System.out.println(c);
         }
         catch(ArithmeticException e){
+//          prints all the exception information
+            e.printStackTrace();
+
+//          prints the information related to exception description and exception name
             System.out.println(e);
+
+//          prints the information related to only the name of the exception that has occured
+            System.out.println(e.getMessage());
         }
     }
 }
