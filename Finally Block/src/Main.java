6b0c6963b451d92662aac4456555ebb6ffb8dcee
SWARAJ RAJENDRA MUTALIK <swarajmutalik@gmail.com>
2024-03-04 22:01:25 +0530

    Finally Block

--- Finally Block/src/Main.java
+++ Finally Block/src/Main.java
@@ -1,17 +1,15 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+        try{
+            int a = 100, b = 0, c;
+            c = a / b;
+            System.out.println(c);
+        }
+        catch(Exception e){
+            System.out.println(e);
+        }
+        finally {
+            System.out.println("Finally Block");
         }
     }
 }
