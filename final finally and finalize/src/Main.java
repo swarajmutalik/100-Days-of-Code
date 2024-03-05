e572e1dfdc47a38d601b985173d497a653933ce4
SWARAJ RAJENDRA MUTALIK <swarajmutalik@gmail.com>
2024-03-05 21:53:16 +0530

    final, finally and finalize

--- final finally and finalize/src/Main.java
+++ final finally and finalize/src/Main.java
@@ -1,17 +1,26 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+//      example of final keyword
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+//        final int a = 10;
+//        a++;
+//        System.out.println(a);
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+//        example of finally block
+
+//        try{
+//            int result = 20 / 0;
+//            System.out.println(result);
+//        }
+//        catch (ArithmeticException e){
+//            System.out.println(e);
+//        }
+
+//        this block of code will be executed no matter what
+//        finally{
+//            System.out.println("finally block");
+//        }
+
     }
+
 }
