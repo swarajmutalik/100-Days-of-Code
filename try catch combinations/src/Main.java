824b43534d5305b3b10fea551ae639d11512f6a1
SWARAJ RAJENDRA MUTALIK <swarajmutalik@gmail.com>
2024-03-09 00:05:18 +0530

    various tr y catch combinations

--- try catch combinations/src/Main.java
+++ try catch combinations/src/Main.java
@@ -1,17 +1,22 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+//        Using only try and catch block
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+//        try{}
+//        catch(Exception e){}
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+//      Using try,catch and finally
+
+//        try{}
+//        catch(Exception e){}
+//        finally {}
+
+//        Using nested try catch
+
+//        try{}
+//        catch(ArithmeticException e){
+//            try {}
+//            catch(Exception e1){}
+//        }
     }
 }
