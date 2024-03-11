0b98103bbe6ff5c2b1ddaeb02399c2e3038c379b
SWARAJ RAJENDRA MUTALIK <swarajmutalik@gmail.com>
2024-03-11 20:53:09 +0530

    throw keyword

--- throw keyword/src/Main.java
+++ throw keyword/src/Main.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+
+class YoungerAgeException extends RuntimeException {
+    YoungerAgeException(String msg){
+        super(msg);
+    }
+}
 public class Main {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the age:");
+
+        int age = sc.nextInt();
+        if(age < 18){
+            throw new YoungerAgeException("You are not eligible for voting");
+        }
+        else{
+            System.out.println("Eligible for voting");
+        }
     }
 }
