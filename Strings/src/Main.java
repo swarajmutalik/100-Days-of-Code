5d3cadf850a2a57844dbe5c9d1b604bacd4fe4af
SWARAJ RAJENDRA MUTALIK <swarajmutalik@gmail.com>
2024-03-15 17:47:09 +0530

    Introduction to Strings

--- Strings/src/Main.java
+++ Strings/src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        
+//        Different ways of creating a String
+        String str = new String("Swaraj");
+        System.out.println(str);
+
+        String name = "Mutalik";
     }
 }
