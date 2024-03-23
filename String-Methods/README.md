95cea627691434b892ff183a60b6bcf0deeabb91
SWARAJ RAJENDRA MUTALIK <swarajmutalik@gmail.com>
2024-03-23 23:40:43 +0530

    String Methods

---
+++ String-Methods/README.md
@@ -0,0 +1,7 @@
+### String Methods 
+
+#### == Vs .equals() method 
+
+== operator is used for reference comparison. It means == operator checks if both the objects point to the same memory location or not.
+
+.equals() method used for content comparison. It means is it used to check object value.
--- String-Methods/src/Main.java
+++ String-Methods/src/Main.java
@@ -1,17 +1,15 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        String s1 = "Hello";
+        String s2 = "Hello";
+        String s3 = new String("Hello");
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+//      == operator
+        System.out.println(s1 == s2);
+        System.out.println(s1 == s3);
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+//      .equals() method
+        System.out.println(s1.equals(s2));
+        System.out.println(s1.equals(s3));
     }
 }
