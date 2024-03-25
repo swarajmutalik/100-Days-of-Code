ed3c7daaac05385b1fa06ef5c0e74e5328038a3b
SWARAJ RAJENDRA MUTALIK <swarajmutalik@gmail.com>
2024-03-25 19:59:31 +0530

    String Methods

--- String-Methods/src/Main.java
+++ String-Methods/src/Main.java
@@ -3,13 +3,34 @@ public class Main {
         String s1 = "Hello";
         String s2 = "Hello";
         String s3 = new String("Hello");
+        String s4 = "";
+        String s5 = "  abc   ";
 
 //      == operator
-        System.out.println(s1 == s2);
-        System.out.println(s1 == s3);
+//        System.out.println(s1 == s2);
+//        System.out.println(s1 == s3);
+//
+////      .equals() method
+//        System.out.println(s1.equals(s2));
+//        System.out.println(s1.equals(s3));
 
-//      .equals() method
-        System.out.println(s1.equals(s2));
-        System.out.println(s1.equals(s3));
-    }
+//       .length() method
+        System.out.println(s1.length());
+        int i = s1.length();
+        if(i == 0){
+            System.out.println("String is empty");
+        }
+
+//        .isEmpty() method
+        System.out.println(s1.isEmpty());
+
+        if(s4.isEmpty() == true){
+            System.out.println("String is empty");
+        }
+
+//        .trim() method
+        System.out.println(s5);
+        System.out.println(s5.trim());
+
+     }
 }
